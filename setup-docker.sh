8a7e286c0090df7f492b9849881ea5f7b4bdae7e
chungmittu <31134373+chungmittu@users.noreply.github.com>
2023-11-13 13:23:55 +0700

    Add files via upload

---
+++ setup-docker.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+sudo timedatectl set-timezone Asia/Ho_Chi_Minh
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+cat > /etc/docker/daemon.json <<EOF
+{
+  "insecure-registries" : ["dockerhub.example.lan"],
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m",
+    "max-file": "10"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+
+sudo systemctl status docker
+sudo systemctl enable docker
+
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+echo "setup done!"
